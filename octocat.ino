ec57007856a50b83c6de6c97554bbf34ccb31855
Johnny Goodnow <jgoodnow@fastmail.com>
2016-03-13 19:57:12 -0700

    Add random sparkles animation and increase int sizes.
    * Any time we're moving around the LED array, use uint16_t.
    * Add random sparkles (group and rainbow) animation.

--- octocat.ino
+++ octocat.ino
@@ -1,12 +1,13 @@
 #include <FastLED.h>
 
+#define BUTTON_PIN 2
 #define CLOCK_PIN 13
 #define DATA_PIN 12
 #define DEFAULT_BRIGHTNESS 191
 #define DEFAULT_HUE 5
 #define DEFAULT_SAT 191
 #define FPS 120
-#define LED_GROUP_SIZE NUM_LEDS / 12
+#define LED_GROUP_SIZE (NUM_LEDS / 12)
 #define MAX_BRIGHTNESS 223
 #define MAX_SAT 255
 #define MIN_BRIGHTNESS 63
@@ -18,23 +19,23 @@
 CRGB leds[NUM_LEDS];
 CHSV hsvs[NUM_LEDS];
 
-const uint8_t LEFT_CENTER = (
+const uint16_t LEFT_CENTER = (
     LED_GROUP_SIZE % 2 == 0 ? (LED_GROUP_SIZE / 2) - 1 : LED_GROUP_SIZE / 2);
-const uint8_t RIGHT_CENTER = LED_GROUP_SIZE / 2;
-const uint8_t DIST_CENTER = (
+const uint16_t RIGHT_CENTER = LED_GROUP_SIZE / 2;
+const uint16_t DIST_CENTER = (
     LED_GROUP_SIZE % 2 == 0 ? LED_GROUP_SIZE / 2 : (LED_GROUP_SIZE / 2) + 1);
 const uint8_t HUE_DISTANCES[NUM_HUE_DISTANCES] = {0, 127, 42, 170, 234, 191};
 
 boolean flashRed = false;
-uint8_t currentPattern = 0;
 uint8_t buttonState;
+uint8_t currentPattern = 0;
 uint8_t lastButtonState = LOW;
-uint64_t lastDebounceTime;
+uint8_t rainbowHue = 0;
 uint64_t debounceDelay = 50;
+uint64_t lastDebounceTime;
 
-uint8_t calcHueForGroup(uint8_t baseHue, uint8_t group) {
-    uint8_t hueDistance = (group / LED_GROUP_SIZE) % NUM_HUE_DISTANCES;
-    return baseHue + HUE_DISTANCES[hueDistance];
+uint8_t calcHueForGroup(uint8_t baseHue, uint16_t group) {
+    return baseHue + HUE_DISTANCES[group % NUM_HUE_DISTANCES];
 }
 
 void turnOffLights() {
@@ -46,15 +47,13 @@ void turnOffLights() {
 }
 
 void setupHuesForGroups() {
-    uint8_t baseHue = DEFAULT_HUE;
     uint8_t hue;
+    uint16_t groupNum;
     for (uint16_t i = 0; i < NUM_LEDS; i += LED_GROUP_SIZE) {
-        hue = calcHueForGroup(baseHue, i);
-        for (uint8_t j = i; j < i + LED_GROUP_SIZE; ++j) {
+        groupNum = i / LED_GROUP_SIZE;
+        hue = calcHueForGroup(DEFAULT_HUE + (63 * groupNum), groupNum);
+        for (uint16_t j = i; j < i + LED_GROUP_SIZE; ++j) {
             hsvs[j] = CHSV(hue, DEFAULT_SAT, 0);
-        }
-        if (i != 0 && (i / LED_GROUP_SIZE) % NUM_HUE_DISTANCES == 0) {
-            baseHue += 63;
         }
     }
 }
@@ -80,18 +79,18 @@ void twinkleLoop() {
     }
 }
 
-void movingLoop() {
+void movingPattern() {
     uint8_t hue;
     setupHuesForGroups();
-    turnOffLights();
-    for (uint8_t i = 0; i < LED_GROUP_SIZE; ++i) {
+    for (uint16_t i = 0; i < LED_GROUP_SIZE; ++i) {
         for (uint16_t j = 0; j < NUM_LEDS; j += LED_GROUP_SIZE) {
             hsvs[j + i].hue += 16;
             hsvs[j + i].sat = MAX_SAT;
             hsvs[j + i].val = DEFAULT_BRIGHTNESS;
-            for (uint8_t k = j; k < j + i; ++k) {
-                hsvs[k].val = lerp8by8(0, hsvs[k].val, 128);
+            for (uint16_t k = j; k < j + i; ++k) {
                 hsvs[k].hue += 16;
+                hsvs[k].sat = lerp8by8(0, hsvs[k].sat, 224);
+                hsvs[k].val = lerp8by8(0, hsvs[k].val, 192);
             }
         }
         hsv2rgb_rainbow(hsvs, leds, NUM_LEDS);
@@ -100,8 +99,8 @@ void movingLoop() {
     }
 }
 
-void panningAnimation(uint8_t left, uint8_t right, uint8_t len, boolean out) {
-    for (uint8_t i = 0; i < len; ++i) {
+void panningAnimation(uint16_t left, uint16_t right, uint16_t len, boolean out) {
+    for (uint16_t i = 0; i < len; ++i) {
         if (i != 0) {
             if (out) {
                 --left, ++right;
@@ -123,7 +122,7 @@ void panningAnimation(uint8_t left, uint8_t right, uint8_t len, boolean out) {
                 hsvs[j + right].val = 0;
             }
         }
-        for (uint8_t j = 0; j < NUM_LEDS; ++j) {
+        for (uint16_t j = 0; j < NUM_LEDS; ++j) {
             hsvs[j].hue += 32;
             if (i % 2 == 0) {
                 hsvs[j].sat -= 32;
@@ -144,7 +143,7 @@ void panningLoop() {
     panningAnimation(LEFT_CENTER, RIGHT_CENTER, DIST_CENTER, true);
 }
 
-void pingPongSide(uint8_t lower, uint8_t upper, int8_t inc) {
+void pingPongSide(uint16_t lower, uint16_t upper, int16_t inc) {
     uint8_t dir = lower + inc;
     boolean out = true;
 
@@ -179,7 +178,7 @@ void pingPongSide(uint8_t lower, uint8_t upper, int8_t inc) {
     }
 }
 
-void pingPongAnimation(uint8_t left, uint8_t right) {
+void pingPongAnimation(uint16_t left, uint16_t right) {
     for (uint16_t j = 0; j < NUM_LEDS; j += LED_GROUP_SIZE) {
         CHSV curHsv = CHSV(
             calcHueForGroup(DEFAULT_HUE, j),
@@ -202,7 +201,7 @@ void pingPongLoop() {
 
 void blinkRedLights() {
     for (uint8_t i = 0; i < 4; ++i) {
-        uint8_t dot = 0;
+        uint16_t dot = 0;
         for (dot = 0; dot < NUM_LEDS; ++dot) {
             leds[dot] = CRGB::Red;
         }
@@ -216,9 +215,33 @@ void blinkRedLights() {
     }
 }
 
+void randomSparklesGroupPattern() {
+    randomSparklesPattern(true);
+}
+
+void randomSparklesRainbowPattern() {
+    randomSparklesPattern(false);
+}
+
+void randomSparklesPattern(boolean groupHues) {
+    uint16_t pos = random16(NUM_LEDS);
+    uint8_t hue;
+
+    fadeToBlackBy(leds, NUM_LEDS, 10);
+    if (groupHues) {
+        hue = calcHueForGroup(
+            DEFAULT_HUE + (63 * (pos / LED_GROUP_SIZE)), pos / LED_GROUP_SIZE);
+    } else {
+        hue = rainbowHue;
+    }
+    leds[pos] += CHSV(hue + random8(64), DEFAULT_SAT, MAX_BRIGHTNESS);
+}
+
+/* Main Logic */
+
 typedef void (*PatternArray[])();
 PatternArray patterns = {
-    twinkleLoop, pingPongLoop, movingLoop, panningLoop
+    randomSparklesGroupPattern, randomSparklesRainbowPattern, movingPattern
 };
 
 void nextPattern() {
@@ -260,4 +283,5 @@ void loop() {
     }
     FastLED.delay(1000 / FPS);
     EVERY_N_SECONDS(5) { nextPattern(); }
+    EVERY_N_MILLISECONDS(20) { ++rainbowHue; }
 }
